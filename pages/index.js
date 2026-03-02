5280a09ade075f6ebd60b19d402d7956c9b6df92
Halie McDonald <halie.mc99@gmail.com>
2026-03-02 09:20:38 -0700

    new test list item

--- pages/index.js
+++ pages/index.js
@@ -36,6 +36,9 @@ export default function Home() {
           <li className="tracking-[-.01em]">
             Save and see your changes instantly.
           </li>
+          <li className="tracking-[-.01em]">
+            Push to GitHub to see new list item.
+          </li>
         </ol>
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
